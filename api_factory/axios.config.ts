354f1ee2b2ceda0b70f0a6a73d98f5ae294141d3
Marquis <abahmarquis@gmail.com>
2026-05-18 14:06:53 +0100

    chore: aggressively point api to live endpoint

--- api_factory/axios.config.ts
+++ api_factory/axios.config.ts
@@ -5,12 +5,10 @@ const { showToast } = useCustomToast();
 
 // const { token, logOut } = useUser(); // REMOVED TOP-LEVEL CALL
 
-const $GATEWAY_ENDPOINT_WITHOUT_VERSION = import.meta.env
-  .VITE_BASE_URL as string;
-const $GATEWAY_ENDPOINT = import.meta.env.VITE_API_BASE_URL + "/api/v1";
-const $GATEWAY_ENDPOINT_V2 = import.meta.env.VITE_API_BASE_URL + "/v2";
-const $IMAGE_UPLOAD_ENDPOINT = import.meta.env
-  .VITE_IMAGE_UPLOAD_BASE_URL as string;
+const $GATEWAY_ENDPOINT_WITHOUT_VERSION = "https://api.erranders.org";
+const $GATEWAY_ENDPOINT = "https://api.erranders.org/api/v1";
+const $GATEWAY_ENDPOINT_V2 = "https://api.erranders.org/v2";
+const $IMAGE_UPLOAD_ENDPOINT = "https://api.erranders.org";
 
 export const GATEWAY_ENDPOINT = axios.create({
   baseURL: $GATEWAY_ENDPOINT,
