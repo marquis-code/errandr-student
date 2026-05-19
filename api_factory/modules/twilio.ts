fd7dc1c479505ee3577548ed90223f031e0b8cc7
Marquis <abahmarquis@gmail.com>
2026-05-19 05:48:47 +0100

    feat: route voice client to Africa's Talking

--- api_factory/modules/twilio.ts
+++ api_factory/modules/twilio.ts
@@ -2,6 +2,6 @@ import { GATEWAY_ENDPOINT_WITH_AUTH } from '../axios.config';
 
 export const twilio_api = {
   makeCall: (to: string, message: string) => {
-    return GATEWAY_ENDPOINT_WITH_AUTH.post('/twilio/make-call', { to, message });
+    return GATEWAY_ENDPOINT_WITH_AUTH.post('/africastalking/make-call', { to, message });
   },
 };
