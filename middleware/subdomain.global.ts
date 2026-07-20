d15b2199482b0a48815991118c88a40e39f35f14
Marquis <abahmarquis@gmail.com>
2026-07-20 17:00:21 +0100

    Add global middleware to redirect non-www to www to fix Firebase auth

--- middleware/subdomain.global.ts
+++ middleware/subdomain.global.ts
@@ -2,6 +2,12 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (process.client) {
     const host = window.location.hostname
     
+    // Aggressive redirect for missing WWW to fix Firebase Auth Authorized Domains issue
+    if (host === 'erranders.org') {
+      window.location.href = `https://www.erranders.org${to.fullPath}`
+      return
+    }
+
     // Ignore vercel deployments from subdomain logic
     if (host.includes('vercel.app')) {
       return
